69030ed3fdd35a267dfa562c46f640ccd8417bc9
jmoncadam <j_moncada98@hotmail.com>
2025-04-03 01:18:06 -0700

    flujo completo

--- src/main.ts
+++ src/main.ts
@@ -18,8 +18,8 @@ async function bootstrap() {
     'http://localhost:3000',
     'http://localhost:3001',
     'http://localhost:4200',
-    'https://app.omenpro.com',
-    'https://api.omenpro.com'
+    'https://deckritual.com',
+    "http://35.93.216.65:3400"
   ];
 
   const corsOptions = {
